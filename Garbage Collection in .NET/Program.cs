d923d414e72b47daac442c7273b228f7edb9ed8a
aishwaryavasu0509-prof <aishwaryanani0509@gmail.com>
2025-02-03 21:47:11 +0530

    garbagecollection

---
+++ Garbage Collection in .NET/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garbage_Collection_in.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating instances of the ResourceHolder class
+            for (int i = 0; i < 5; i++)
+            {
+                ResourceHolder resourceHolder = new ResourceHolder(i);
+                resourceHolder.UseResource();
+            }
+
+            // Forcing garbage collection to demonstrate destructor call
+            Console.WriteLine("Forcing garbage collection...");
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Console.WriteLine("End of Main method.");
+        }
+    }
+
+    class ResourceHolder
+    {
+        private int id;
+
+        // Constructor
+        public ResourceHolder(int id)
+        {
+            this.id = id;
+            Console.WriteLine($"ResourceHolder {id} created.");
+        }
+
+        // Method to simulate resource usage
+        public void UseResource()
+        {
+            Console.WriteLine($"ResourceHolder {id} is using resource.");
+        }
+
+        // Destructor
+        ~ResourceHolder()
+        {
+            Console.WriteLine($"ResourceHolder {id} destructor called. Cleaning up resources.");
+        }
+    }
+}
---
+++ Garbage Collection in .NET/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Garbage Collection in .NET")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Trimble Inc.")]
+[assembly: AssemblyProduct("Garbage Collection in .NET")]
+[assembly: AssemblyCopyright("Copyright © Trimble Inc. 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1205b316-e14c-4749-9868-1cae4db65467")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
