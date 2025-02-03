767f3164dd648c2afa37558b788a4a71845d59cf
aishwaryavasu0509-prof <aishwaryanani0509@gmail.com>
2025-02-03 22:48:39 +0530

    delegates

---
+++ Delegates (Multicast, Generic, and Anonymous Methods)/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates__Multicast__Generic__and_Anonymous_Methods_
+{
+    internal class Program
+    {
+        class Program
+        {
+            // Multicast delegate declaration
+            public delegate void MulticastDelegate(string message);
+
+            static void Main(string[] args)
+            {
+                // Multicast Delegate
+                MulticastDelegate multicast = Method1;
+                multicast += Method2;
+                Console.WriteLine("Multicast Delegate:");
+                multicast("Hello from Multicast Delegate!");
+
+                // Generic Delegate
+                Action<string> genericDelegate = GenericMethod;
+                Console.WriteLine("\nGeneric Delegate:");
+                genericDelegate("Hello from Generic Delegate!");
+
+                // Anonymous Method
+                Action<string> anonymousMethod = delegate (string message)
+                {
+                    Console.WriteLine($"Anonymous Method received: {message}");
+                };
+                Console.WriteLine("\nAnonymous Method:");
+                anonymousMethod("Hello from Anonymous Method!");
+
+                // Lambda Expression as Anonymous Method
+                Action<string> lambdaExpression = (message) => Console.WriteLine($"Lambda Expression received: {message}");
+                Console.WriteLine("\nLambda Expression:");
+                lambdaExpression("Hello from Lambda Expression!");
+            }
+
+            static void Method1(string message)
+            {
+                Console.WriteLine($"Method1 received: {message}");
+            }
+
+            static void Method2(string message)
+            {
+                Console.WriteLine($"Method2 received: {message}");
+            }
+
+            static void GenericMethod(string message)
+            {
+                Console.WriteLine($"GenericMethod received: {message}");
+            }
+        }
+    }
---
+++ Delegates (Multicast, Generic, and Anonymous Methods)/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Delegates (Multicast, Generic, and Anonymous Methods)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Trimble Inc.")]
+[assembly: AssemblyProduct("Delegates (Multicast, Generic, and Anonymous Methods)")]
+[assembly: AssemblyCopyright("Copyright © Trimble Inc. 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cf70427d-7fb0-4870-8aed-65d0893b60eb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
