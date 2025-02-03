755163b1ea9efe1b529b674c5ed86f682985b2ef
aishwaryavasu0509-prof <aishwaryanani0509@gmail.com>
2025-02-03 20:03:22 +0530

    badic structure of c# program

---
+++ basicstructureofc#program/Program.cs
@@ -0,0 +1,20 @@
+﻿//system class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//class containing the function
+namespace basicstructureofc_program
+{
+    internal class Program
+    {
+        //entry point of c#
+        static void Main(string[] args)
+        {
+            // executions
+            Console.WriteLine("Hello world");
+        }
+    }
+}
