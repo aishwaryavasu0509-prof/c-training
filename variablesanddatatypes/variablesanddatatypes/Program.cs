850720ab6327117d0655bc9fa9c2436b11f80ff0
aishwaryavasu0509-prof <aishwaryanani0509@gmail.com>
2025-02-03 20:39:24 +0530

    variables and data types

---
+++ variablesanddatatypes/variablesanddatatypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variablesanddatatypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int value
+            int age = 30;
+            char dummy = 'a';
+            float pi = 3.14e30f;
+            double pii = 3.14e40;
+            string name = "ash";
+            Console.WriteLine(name);
+            Console.WriteLine(dummy);
+            Console.WriteLine(pi);
+            Console.WriteLine(pii);
+            Console.WriteLine(age);
+        }
+    }
+}
