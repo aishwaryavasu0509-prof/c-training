80e111ae612332c91fed0c557b80bccc72bbde2d
aishwaryavasu0509-prof <aishwaryanani0509@gmail.com>
2025-02-03 22:00:07 +0530

    abstractclassandinterface

---
+++ Abstract Class and Interface/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_and_Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating instances of the concrete classes
+            Car car = new Car("Toyota", "Camry", 2020);
+            Bicycle bicycle = new Bicycle("Giant", "Escape 3");
+
+            // Using the methods defined in the abstract class and interface
+            car.DisplayInfo();
+            car.StartEngine();
+            car.Drive();
+
+            bicycle.DisplayInfo();
+            bicycle.Drive();
+        }
+    }
+
+    // Abstract class
+    abstract class Vehicle
+    {
+        // Properties
+        public string Make { get; }
+        public string Model { get; }
+
+        // Constructor
+        public Vehicle(string make, string model)
+        {
+            Make = make;
+            Model = model;
+        }
+
+        // Abstract method
+        public abstract void DisplayInfo();
+    }
+
+    // Interface
+    interface IDriveable
+    {
+        void Drive();
+    }
+
+    // Concrete class inheriting from abstract class and implementing interface
+    class Car : Vehicle, IDriveable
+    {
+        public int Year { get; }
+
+        // Constructor
+        public Car(string make, string model, int year)
+            : base(make, model)
+        {
+            Year = year;
+        }
+
+        // Implementing abstract method
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Car: {Make} {Model}, Year: {Year}");
+        }
+
+        // Additional method
+        public void StartEngine()
+        {
+            Console.WriteLine($"{Make} {Model} engine started.");
+        }
+
+        // Implementing interface method
+        public void Drive()
+        {
+            Console.WriteLine($"{Make} {Model} is driving.");
+        }
+    }
+
+    // Another concrete class inheriting from abstract class and implementing interface
+    class Bicycle : Vehicle, IDriveable
+    {
+        // Constructor
+        public Bicycle(string make, string model)
+            : base(make, model)
+        {
+        }
+
+        // Implementing abstract method
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Bicycle: {Make} {Model}");
+        }
+
+        // Implementing interface method
+        public void Drive()
+        {
+            Console.WriteLine($"{Make} {Model} is being ridden.");
+        }
+    }
+}
---
+++ Abstract Class and Interface/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Abstract Class and Interface")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Trimble Inc.")]
+[assembly: AssemblyProduct("Abstract Class and Interface")]
+[assembly: AssemblyCopyright("Copyright © Trimble Inc. 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1cd757cc-4628-499f-927d-d33b71ce43db")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
