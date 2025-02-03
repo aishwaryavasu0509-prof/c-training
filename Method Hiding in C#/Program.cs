57c3ad8bd3558547aa1d9e6d923871d18f01f29d
aishwaryavasu0509-prof <aishwaryanani0509@gmail.com>
2025-02-03 22:09:38 +0530

    methodhiding

---
+++ Method Hiding in C#/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Hiding_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an instance of the base class
+            BaseClass baseClass = new BaseClass();
+            baseClass.Display();
+
+            // Creating an instance of the derived class
+            DerivedClass derivedClass = new DerivedClass();
+            derivedClass.Display();
+
+            // Using a base class reference to a derived class object
+            BaseClass baseRefToDerived = new DerivedClass();
+            baseRefToDerived.Display();
+        }
+    }
+
+    class BaseClass
+    {
+        public void Display()
+        {
+            Console.WriteLine("Display method in BaseClass.");
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public new void Display()
+        {
+            Console.WriteLine("Display method in DerivedClass.");
+        }
+    }
+}
