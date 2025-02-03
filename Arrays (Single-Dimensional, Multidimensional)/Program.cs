d84770cc7bd3500d5976571924fabfb78cfa8b94
aishwaryavasu0509-prof <aishwaryanani0509@gmail.com>
2025-02-03 22:40:16 +0530

    arrays

---
+++ Arrays (Single-Dimensional, Multidimensional)/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays__Single_Dimensional__Multidimensional_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Single-Dimensional Array
+            int[] singleDimensionalArray = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Single-Dimensional Array:");
+            for (int i = 0; i < singleDimensionalArray.Length; i++)
+            {
+                Console.WriteLine($"Element at index {i}: {singleDimensionalArray[i]}");
+            }
+
+            // Multidimensional Array (2D Array)
+            int[,] multiDimensionalArray = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
+            Console.WriteLine("\nMultidimensional Array (2D Array):");
+            for (int i = 0; i < multiDimensionalArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < multiDimensionalArray.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Element at index [{i},{j}]: {multiDimensionalArray[i, j]}");
+                }
+            }
+
+            // Jagged Array
+            int[][] jaggedArray = new int[3][];
+            jaggedArray[0] = new int[] { 1, 2 };
+            jaggedArray[1] = new int[] { 3, 4, 5 };
+            jaggedArray[2] = new int[] { 6, 7, 8, 9 };
+            Console.WriteLine("\nJagged Array:");
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                Console.Write($"Row {i}: ");
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write($"{jaggedArray[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Arrays (Single-Dimensional, Multidimensional)/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Arrays (Single-Dimensional, Multidimensional)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Trimble Inc.")]
+[assembly: AssemblyProduct("Arrays (Single-Dimensional, Multidimensional)")]
+[assembly: AssemblyCopyright("Copyright © Trimble Inc. 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3304a95b-6d6c-45ea-909e-fd20666a8e39")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
