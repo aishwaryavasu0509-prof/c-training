cab4d5d450b3093d264c131e8d31414192d0ad4a
aishwaryavasu0509-prof <aishwaryanani0509@gmail.com>
2025-02-03 21:21:00 +0530

    string manipulation

---
+++ stringmanipulation/stringmanipulation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace stringmanipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user to input a string
+            Console.WriteLine("Please input the string:");
+            string inputString = Console.ReadLine();
+
+            // Convert the input string to uppercase
+            string upperString = inputString.ToUpper();
+
+            // Reverse the string
+            char[] charArray = inputString.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedString = new string(charArray);
+
+            // Replace all spaces in the string with underscores (_)
+            string replacedString = inputString.Replace(' ', '_');
+
+            // Display the original and manipulated strings
+            Console.WriteLine("Original String: " + inputString);
+            Console.WriteLine("Uppercase: " + upperString);
+            Console.WriteLine("Reversed: " + reversedString);
+            Console.WriteLine("Spaces Replaced: " + replacedString);
+
+            // Additional feature: Count the number of characters and words in the string
+            int charCount = inputString.Length;
+            int wordCount = inputString.Split(new char[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
+
+            Console.WriteLine("Number of Characters: " + charCount);
+            Console.WriteLine("Number of Words: " + wordCount);
+
+            // Wait for user input before closing the console window
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ stringmanipulation/stringmanipulation/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("stringmanipulation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Trimble Inc.")]
+[assembly: AssemblyProduct("stringmanipulation")]
+[assembly: AssemblyCopyright("Copyright © Trimble Inc. 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6e4c79a8-7ced-4272-a6a9-3e7c6bf456fe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
